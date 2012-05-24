3d9ba1f8f61285d2009083da7c8e3db0cb095726
Benjamin Quorning <bquorning@zendesk.com>
2012-05-24 11:51:11 +0200

    Ruby 1.8 does not like \u . Use ascii characters instead

--- lib/omglog.rb
+++ lib/omglog.rb
@@ -13,8 +13,8 @@ module Omglog
     CLEAR = "\n----\n"
     YELLOW, BLUE, GREY, HIGHLIGHT = '0;33', '0;34', '0;90', '1;30;47'
     SHORTEST_MESSAGE = 12
-    LOG_CMD = %{git log --all --date-order --graph --color --pretty="format: \2%h\3\2%d\3\2 %an, %ar\3\2 %s\3"}
-    LOG_REGEX = /(.*)\u0002(.*)\u0003\u0002(.*)\u0003\u0002(.*)\u0003\u0002(.*)\u0003/
+    LOG_CMD = %{git log --all --date-order --graph --color --pretty="format: --2--%h--3----2--%d--3----2-- %an, %ar--3----2-- %s--3--"}
+    LOG_REGEX = /(.*)--2--(.*)--3----2--(.*)--3----2--(.*)--3----2--(.*)--3--/
 
     def self.run
       rows, cols = `tput lines; tput cols`.scan(/\d+/).map(&:to_i)
